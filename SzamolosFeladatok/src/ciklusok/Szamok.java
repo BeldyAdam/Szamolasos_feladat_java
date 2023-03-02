a691314b3eb9c7e08fa4a3eefcf1c87f811e43d0
BeldyAdam <beldy.adam@gmail.com>
2023-03-02 16:22:17 +0100

    ciklus keszen

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,17 +1,17 @@
 package ciklusok;
-
+//ez
 /**
  *
  * @author Pizza Hut
  */
 public class Szamok {
     public static void main(String[] args) {
-     for (int k = 2; k < 3; k++){
-      for (int j = 2; j < 3; j++){
-        for (int i = 2; i < 5; i++){
-            System.out.println(k+""+j+""+i);
+        for (int k = 2; k <= 4; k++) {
+            for (int i = 2; i <= 4; i++) {
+                for (int j = 2; j <= 4; j++) {
+                    System.out.printf("%d%d%d\n",k, i, j);
         }
+      }
     }
-    }
-}
+  }
 }
